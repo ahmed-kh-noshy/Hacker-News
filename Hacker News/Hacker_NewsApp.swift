1a3c34ec3258c4ad95889be0b93347a11d1366c4
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-08-17 00:04:32 +0300

    adding project files

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by ahmed khaled on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
